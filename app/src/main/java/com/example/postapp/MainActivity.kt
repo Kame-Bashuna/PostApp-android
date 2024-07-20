2427805fac6d3ad9a22f2cba8b847df1119d12f6
Kame-Bashuna <basshunakameroba@gmail.com>
2024-07-20 12:05:21 +0300

    add file

--- app/src/main/java/com/example/postapp/MainActivity.kt
+++ app/src/main/java/com/example/postapp/MainActivity.kt
@@ -8,44 +8,41 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.postapp.databinding.ActivityMainBinding
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-
-
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     lateinit var recyclerView: RecyclerView
-    lateinit var clientPostAdapter: ClientPostAdapter
+    lateinit var postAdapter: ClientPostAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         fetchPosts()
-        recyclerView=findViewById(R.id.wxRecycle)
-        recyclerView.layoutManager=LinearLayoutManager(this)
-
+        recyclerView = binding.wxRecycle
+        recyclerView.layoutManager = LinearLayoutManager(this)
     }
-    fun fetchPosts(){
-        val apiInterface =
-            ApiClient.buildApiInterface(PostApiInterface::class.java)
+    fun fetchPosts() {
+        val apiInterface = ApiClient.buildApiInterface(PostApiInterface::class.java)
         val request = apiInterface.fetchPosts()
-        request.enqueue(object :Callback<List<Post>>{
-            override fun onResponse(p0: Call<List<Post>>, p1: Response<List<Post>>) {
-                if(p1.isSuccessful){
-                    val posts = p1.body()
-                    posts?.let{
-                        clientPostAdapter=ClientPostAdapter(it)
-                        recyclerView.adapter=clientPostAdapter
+        request.enqueue(object : Callback<List<Post>> {
+            override fun onResponse(call: Call<List<Post>>, response: Response<List<Post>>) {
+                if (response.isSuccessful) {
+                    val posts = response.body()
+                    posts?.let {
+                        postAdapter = ClientPostAdapter(it)
+                        recyclerView.adapter = postAdapter
                     }
-                    Toast.makeText(baseContext, "fetched ${posts!!.size} posts", Toast.LENGTH_LONG).show()
-                }
-                else{
-                    Toast.makeText(baseContext,p1.errorBody()?.string(), Toast.LENGTH_LONG).show()
+                    Toast.makeText(baseContext, "Fetched ${posts!!.size} posts", Toast.LENGTH_LONG).show()
+                } else {
+                    Toast.makeText(baseContext, response.errorBody()?.string(), Toast.LENGTH_LONG).show()
                 }
             }
-
-            override fun onFailure(p0: Call<List<Post>>, p1: Throwable) {
-                Toast.makeText(baseContext,p1.message, Toast.LENGTH_SHORT).show()
+            override fun onFailure(call: Call<List<Post>>, t: Throwable) {
+                Toast.makeText(baseContext, t.message, Toast.LENGTH_SHORT).show()
             }
         })
     }
